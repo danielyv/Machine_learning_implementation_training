695efcb1a64a9ce39ec5c47dfdffb83be42121db
danielyv <yves.daniel11@gmail.com>
2018-06-13 15:53:14 +0200

    Entropy functions added

--- src/main/java/Regression/DTR.kt
+++ src/main/java/Regression/DTR.kt
@@ -1,13 +1,14 @@
 package Regression
 //WIP
-import krangl.DataFrame
-import krangl.gt
-import krangl.mean
-import krangl.le
-class DTR(data: DataFrame, name: String,res:Boolean) {
+import krangl.*
+import org.apache.commons.math3.stat.Frequency
+import kotlin.math.log
+
+class DTR(data: DataFrame, name: String,accValue:Double) {
     private val name= name
     private val data:DataFrame = data
-    private val isRes:Boolean=res
+    private var isRes:Boolean=false
+    private  val accuracyValue=accValue
     private var split:Pair<Int,Double> = Pair(0,0.0)
     private var left:DTR?=null
     private var right:DTR?=null
@@ -21,31 +22,44 @@ class DTR(data: DataFrame, name: String,res:Boolean) {
             return (right as DTR).predict(X)
         }
     }
-    public fun splitValue(){
-        var varianceList:MutableList<Pair<String,Double>> = mutableListOf()
-        for(i in data.remove(name).cols){
-            var mean=i.mean() as Double
-            varianceList.add(Pair(i.name,(i.minus(mean).mean() as Double)))
-        }
-        varianceList.sortDescending()
+
+    public fun splitValue(i:Int){
 
     }
+
     public fun splitData():Pair<DataFrame,DataFrame>{
         var f=data.filter { df[split.first] gt split.second }
         var s=data.filter { df[split.first] le split.second }
         return Pair(f,s)
     }
     public fun generateSubTrees(){
+        var entr:DoubleArray= proba_entropy_array()
+        isRes=entr.max() as Double>accuracyValue
         if(isRes){
-            splitValue()
+            splitValue(entr.indexOf(entr.max() as Double))
             val splitdata=splitData()
-            val i1 :Boolean=true
-            val i2 :Boolean=true
-
-            left=DTR(splitdata.second,name,i1)
-            right=DTR(splitdata.first,name,i2)
+            left=DTR(splitdata.second,name,accuracyValue)
+            right=DTR(splitdata.first,name,accuracyValue)
             (left as DTR).generateSubTrees()
             (right as DTR).generateSubTrees()
         }
+    }
+    private fun proba_entropy_array():DoubleArray{
+        var arr:MutableList<Double> = mutableListOf()
+        for(i in data.remove(name).names){
+            arr.add(proba_entropy(data[i],2))
+        }
+        return arr.toDoubleArray()
+    }
+    private fun proba_entropy(col:DataCol,b:Int):Double{
+        var result:Double=0.0
+        var fr:Frequency= Frequency()
+        col.values().forEach { e->fr.addValue(e.toString().toDouble()) }
+        for(i in fr.valuesIterator()){
+            //To rework
+            val pi=fr.getCount(i.toString().toDouble())/col.length.toDouble()
+            result+=(pi* log(pi,b.toDouble()))
+        }
+        return -result
     }
 }
